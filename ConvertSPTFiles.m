24e2b26885552eabf73e7e4d9048d8bbf3ca3603
davidcchristie <67275413+davidcchristie@users.noreply.github.com>
2023-06-30 12:07:45 +0100

    Add files via upload

--- ConvertSPTFiles.m
+++ ConvertSPTFiles.m
@@ -1,9 +1,13 @@
 tolerance = 1e-3;
 UserSettings;
-addpath([pwd,filesep,'functions']);
+% addpath([pwd,filesep,'functions']);
+addpath('functions');
 
 
+% Do we keep track of (and output) the validation info?
+genValFile = genValidationFile >= 1;
 
+    
 
 % Get list of SPT files
 [SPTFileList, SPTFilePath] = GetFileList;
@@ -16,21 +20,42 @@ DirBins = MakeDirBins([ NumberofDirectionalBins, StartDirectionDegrees]);
 
 nfiles = length(SPTFileList);
 
-tic;
+if genValidationFile == 3
+    [target_m1m2n2_all, recalculated_m1m2n2_all] = deal(cell(nfiles,1));
+end
+
+tOverall = tic;
 % Start to convert spectra
 for fileno = 1:nfiles
-    disp(['Reading SPT File ', SPTFileList{fileno}]);
+   
+%     disp(['Reading SPT File ', SPTFileList{fileno}]);
     specDW = ReadSPTFile([SPTFilePath, SPTFileList{fileno}]);
-    targMoms = SprSkewKur2Moms(specDW);
-    momsOK = KrogstadTest(targMoms(:,end-2:end));
+    targetMoms = SprSkewKur2Moms(specDW);
+    momsOK = KrogstadTest(targetMoms(:,end-2:end));
     if any(~momsOK) 
         warning('Some supplied moments are not moments of a non-negative distribution: will return NaNs');
-        targMoms(~momsOK,:) = NaN;    
+        targetMoms(~momsOK,:) = NaN;    
     end
-
-    dirSpec =  Moments2Spec(targMoms, DirBins, tolerance);
-	OutFileName = strsplit(SPTFileList{fileno}, '.');
-    disp('Writing Output File');
+    
+	OutFileName = strsplit(SPTFileList{fileno}, '.');    
+    if genValFile
+        tic
+        [dirSpec, recalculated_m1m2n2] =  Moments2SpecValidate(targetMoms, DirBins, tolerance);
+        timeInSeconds = toc;
+        target_m1m2n2 = targetMoms(:,end-2:end);
+        if genValidationFile == 2
+        save([OutFilePath, filesep, OutFileName{end-1}, '_Validation.mat'], 'target_m1m2n2', 'recalculated_m1m2n2', 'timeInSeconds'); 
+        else
+            target_m1m2n2_all{fileno} = target_m1m2n2;
+            recalculated_m1m2n2_all{fileno} = recalculated_m1m2n2;
+        end
+            
+    else
+        dirSpec =  Moments2SpecValidate(targetMoms, DirBins, tolerance);
+    end
+     if ~mod(fileno, progCountInterval)
+         disp(['Writing Output File ', num2str(fileno), ' / ', num2str(nfiles)]);
+     end
     writematrix( dirSpec, [OutFilePath, filesep, OutFileName{end-1}, '_2D.dat']);    
     % Write direction, freq vectors in separate files?
     % BinFileType = 1: only first time; 2: every time
@@ -44,7 +69,11 @@ end
 
 
 
-totalTime = toc;
+totalTime = toc(tOverall);
+if genValidationFile == 1
+   save([OutFilePath, filesep, OutFileName{end-1}, 'ValidationAll.mat'], ...
+       'target_m1m2n2_all', 'recalculated_m1m2n2_all', 'totalTime', 'nfiles');       
+end
 
 disp(['Complete: Generated ', num2str(nfiles), ' directional spectra in ', num2str(totalTime), ' secs.']);
 disp('MaxEntropySPT by David Christie, Bangor University d.christie@bangor.ac.uk');
